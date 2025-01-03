dc4e9447f661b73f76c81e11961351472765332e
Minou <am.mihoubi@gmail.com>
2025-01-03 19:01:11 +0100

    Generic Views | ListAPIView & RetrieveAPIView

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path  # Importing the path function to define URL patterns.
-from .views import product_list, product_detail, order_list, product_info  # Importing view functions for the API.
+from .views import product_info, ProductList, ProductDetail, OrderList # Importing view functions for the API.
 
 # Define URL patterns for the API endpoints.
 urlpatterns = [
-    path('products/', product_list),  # Endpoint to list all products.
+    path('products/', ProductList.as_view()),  # Endpoint to list all products.
+    path('products/<int:product_id>/', ProductDetail.as_view()),  # Endpoint to retrieve details of a specific product by its ID (pk).
     path('product_info/', product_info),  # Endpoint to list all products.
-    path('products/<int:pk>/', product_detail),  # Endpoint to retrieve details of a specific product by its ID (pk).
-    path('orders/', order_list),  # Endpoint to list all orders.
+    path('orders/', OrderList.as_view()),  # Endpoint to list all orders.
 ]
--- api/views.py
+++ api/views.py
@@ -5,37 +5,25 @@ from .models import Product, Order  # Importing Product and Order models.
 from rest_framework.response import Response  # DRF's Response class to standardize API responses.
 from rest_framework.decorators import api_view  # Decorator to define API views supporting specific HTTP methods.
 from django.db.models import Max  # Importing Max aggregation function for database queries.
+from rest_framework import generics # Importing generics for class-based views.
 
 
 ##### PRODUCTS VIEWS #####
-@api_view(['GET'])  # Specifies that this view only handles GET requests.
-def product_list(request):
-    # Retrieve all products from the database.
-    products = Product.objects.all()
-    # Serialize the list of products.
-    serializer = ProductSerializer(products, many=True)
-    # Return the serialized data as a response.
-    # return JsonResponse({'data': serializer.data})  # Alternative way to return JSON data.
-    return Response(serializer.data)
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.exclude(stock__gt=0)  # Queryset to retrieve all products from the database.
+    serializer_class = ProductSerializer
 
 
 ##### PRODUCT DETAIL VIEW #####
-@api_view(['GET'])
-def product_detail(request, pk):
-    # Retrieve a product by its primary key or return a 404 error if not found.
-    # product = Product.objects.get(id=pk)  # Direct retrieval without 404 handling.
-    product = get_object_or_404(Product, id=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
-
-
+class ProductDetail(generics.RetrieveAPIView):
+    queryset = Product.objects.all()  # Queryset to retrieve all products from the database.
+    serializer_class = ProductSerializer # Serializer class to use for serializing the retrieved product.
+    lookup_url_kwarg = 'product_id'  # Custom URL keyword argument to retrieve the product ID from the URL.
 
 ##### ORDERS VIEWS #####
-@api_view(['GET'])  
-def order_list(request):
-    orders = Order.objects.prefetch_related('items__product')
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
+class OrderList(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')  # Queryset to retrieve all orders with related items and products.
+    serializer_class = OrderSerializer # Serializer class to use for serializing the retrieved orders.
 
 
 
@@ -50,3 +38,28 @@ def product_info(request):
         'total_stock_value': sum(p.price * p.stock for p in products),
     })
     return Response(serializer.data)
+
+
+'''
+@api_view(['GET'])  # Specifies that this view only handles GET requests.
+def product_list(request):
+    products = Product.objects.all() # Retrieve all products from the database.
+    serializer = ProductSerializer(products, many=True) # Serialize the list of products.
+    return Response(serializer.data) # Return the serialized data as a response. return JsonResponse({'data': serializer.data})  # Alternative way to return JSON data.
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    # Retrieve a product by its primary key or return a 404 error if not found.
+    # product = Product.objects.get(id=pk)  # Direct retrieval without 404 handling.
+    product = get_object_or_404(Product, id=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])  
+def order_list(request):
+    orders = Order.objects.prefetch_related('items__product')
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+'''
